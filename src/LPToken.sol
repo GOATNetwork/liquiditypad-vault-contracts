957ddd892840db1219da521ee05b8b47c4e60de5
Li-Qing Wang <liqingnz@gmail.com>
2025-02-18 23:27:35 +0800

    fix: add whitelist to transfer calls

--- src/LPToken.sol
+++ src/LPToken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: BUSL-1.1
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
@@ -10,12 +10,14 @@ contract LPToken is AccessControl, ERC20 {
     bytes32 public constant MINT_ROLE = keccak256("MINT_ROLE");
 
     bool public transferSwitch;
+    mapping(address => bool) public transferWhitelist;
 
     constructor(
         string memory _name,
         string memory _symbol
     ) ERC20(_name, _symbol) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        transferSwitch = false;
     }
 
     function setTransferSwitch(
@@ -23,6 +25,13 @@ contract LPToken is AccessControl, ERC20 {
     ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         transferSwitch = _transferSwitch;
         emit SetTransferSwitch(_transferSwitch);
+    }
+
+    function setTransferWhitelist(
+        address _address,
+        bool _isWhitelisted
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        transferWhitelist[_address] = _isWhitelisted;
     }
 
     function mint(address _to, uint256 _amount) external onlyRole(MINT_ROLE) {
@@ -37,7 +46,10 @@ contract LPToken is AccessControl, ERC20 {
         address to,
         uint256 value
     ) public override returns (bool) {
-        require(transferSwitch, "LPToken: transfer is paused");
+        require(
+            transferSwitch || transferWhitelist[msg.sender],
+            "LPToken: transfer is paused"
+        );
         return super.transfer(to, value);
     }
 
@@ -46,7 +58,10 @@ contract LPToken is AccessControl, ERC20 {
         address to,
         uint256 value
     ) public override returns (bool) {
-        require(transferSwitch, "LPToken: transfer is paused");
+        require(
+            transferSwitch || transferWhitelist[msg.sender],
+            "LPToken: transfer is paused"
+        );
         return super.transferFrom(from, to, value);
     }
 }
